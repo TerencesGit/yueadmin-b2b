25753f0a852d6fe55a8119a4362902403a4e2368
transformGit <2630243397@qq.com>
2017-08-29 18:53:14 +0800

    add gh-pages

---
+++ static/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bdb83b3f13d447423763c632f08eb139";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
